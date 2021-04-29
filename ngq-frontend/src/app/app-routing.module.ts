dde9c5bfcec461af5bb41da822fdee95d818baa6
Slimaine <slimaine.bensadoun@gmail.com>
2021-04-29 20:05:35 +0200

    Design review

--- ngq-frontend/src/app/app-routing.module.ts
+++ ngq-frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import { HomeComponent } from './home';
 import { AuthGuard } from './_helpers';
+import { AddEditComponent } from './users/add-edit.component';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const usersModule = () => import('./users/users.module').then(x => x.UsersModule);
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent,canActivate: [AuthGuard]  },
   { path: 'detail/:id', component: HeroDetailComponent,canActivate: [AuthGuard]  },
   { path: 'heroes', component: HeroesComponent,canActivate: [AuthGuard]  },
+  { path: 'add-user', component: AddEditComponent,canActivate: [AuthGuard]  },
   { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
   { path: 'account', loadChildren: accountModule },
       // otherwise redirect to home
