d7a3e3ec03f7b96a5833ca558a4a4b59dba7fd96
Slimaine <slimaine.bensadoun@gmail.com>
2021-05-04 18:25:17 +0200

    Adding defult users

--- ngq-backend/src/main/java/com/slim/ngq/AppLifecycleBean.java
+++ ngq-backend/src/main/java/com/slim/ngq/AppLifecycleBean.java
@@ -38,8 +38,8 @@ public class AppLifecycleBean {
         		new Hero("Nail"),
         		new Hero("Jean")));
         
-        userRepository.persist(new User("admin",  "Administrator", "Administrator","admin", null));
-        userRepository.persist(new User("user",  "User", "User","user", null));
+        userRepository.persist(new User("admin",  "admin", "Administrator","Administrator", null));
+        userRepository.persist(new User("user",  "admin", "User","User", null));
         
     }
 
